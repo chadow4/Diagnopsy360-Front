7a776d97781020243b2a164784dfe025168363fc
julien <julien.sanchez74@orange.fr>
2023-10-05 01:27:44 +0100

    feat : create user model

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,43 @@
+
+export interface UserCreateDto {
+  firstname: string;
+  lastname: string;
+  email: string;
+  password: string;
+}
+
+export interface UserUpdateDto {
+  firstname?: string;
+  lastname?: string;
+  email?: string;
+  password?: string;
+}
+
+export interface UserDto {
+  id: number;
+  firstname: string;
+  lastname: string;
+  email: string;
+  role: string;
+}
+export interface UserDeleteDto {
+  id: number;
+}
+
+export interface UserLoginDto {
+  email: string;
+  password: string;
+}
+
+export interface UserJWtTokenDto {
+  expiresIn: string;
+  accessToken: string;
+}
+
+
+export interface UserJwtSessionDto{
+  id: number;
+  email: string;
+  role: string;
+}
+
