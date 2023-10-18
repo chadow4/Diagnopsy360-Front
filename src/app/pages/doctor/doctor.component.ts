95f8c0b39eea06a68df3b5b188b59d63654c5f7b
joshua <joshua@localhost.localdomain>
2023-10-18 16:37:24 +0100

    liste client

--- src/app/pages/doctor/doctor.component.ts
+++ src/app/pages/doctor/doctor.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
-import {AuthService} from "../../services/auth.service";
 import {Flowbite} from "../../decorators/flowbite";
+import { UserDto } from '../../models/user.model';
+import {AlertService} from "../../services/alert.service";
+import {UserService} from "../../services/user.service";  
 
 @Flowbite()
 @Component({
@@ -9,13 +11,20 @@ import {Flowbite} from "../../decorators/flowbite";
   styleUrls: ['./doctor.component.scss']
 })
 export class DoctorComponent implements OnInit {
+ 
+  listClient: UserDto[] = []; // Liste pour stocker les utilisateurs avec le rôle 'client'
 
-
-  constructor(private authService: AuthService) {
-
-  }
+  constructor(private authService : AlertService,
+              private userService: UserService) { }
 
   ngOnInit() {
+    this.loadClients();
+  }
+
+  loadClients() {
+    this.userService.getAllUsers().subscribe(users => {
+      this.listClient = users.filter(user => user.role === 'admin'); //TODO replace by client
+    });
   }
 
 
